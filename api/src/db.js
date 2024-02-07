6d90c221e1c45f1c7dd7ece3433346fb98227cde
TebanCardona <estebanfabi737@gmail.com>
2024-02-07 15:40:20 -0500

    test

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const { DATABASE_URL } = process.env;
-
+console.log(DATABASE_URL);
 const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
