af21eafa388823466194fb4a08a1f79d551bcf6a
TebanCardona <estebanfabi737@gmail.com>
2024-02-07 15:23:16 -0500

    change port

--- api/index.js
+++ api/index.js
@@ -14,8 +14,9 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
+const port = process.env.PORT || 3001;
 conn.sync().then(() => {
-  server.listen(process.env.PORT || 3000, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
