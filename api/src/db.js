c3b2ede66f4e973fa51c5972eec8c44ead6f43be
TebanCardona <estebanfabi737@gmail.com>
2024-02-07 15:54:29 -0500

    test3

--- api/src/db.js
+++ api/src/db.js
@@ -4,10 +4,7 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 console.log(DATABASE_URL);
-const sequelize = new Sequelize(DATABASE_URL, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+const sequelize = new Sequelize(DATABASE_URL);
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
