63a7cc2aa84ab6b6b37b8cb868542ed6b6e02334
TebanCardona <estebanfabi737@gmail.com>
2024-02-07 16:06:49 -0500

    test 5

--- api/src/db.js
+++ api/src/db.js
@@ -18,6 +18,7 @@ fs.readdirSync(path.join(__dirname, "/models"))
     modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
+const sequelize = new Sequelize(DATABASE_URL);
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
@@ -26,7 +27,6 @@ let capsEntries = entries.map((entry) => [
   entry[0][0].toUpperCase() + entry[0].slice(1),
   entry[1],
 ]);
-const sequelize = new Sequelize(DATABASE_URL);
 sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
