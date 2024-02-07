7c8a6c395433393d2e68d9ebdb8c48e7c93a5c36
TebanCardona <estebanfabi737@gmail.com>
2024-02-07 15:01:49 -0500

    ready to deploy

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
 conn.sync().then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT || 3000, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const games = require("./videogames.js");
 const genres = require("./genres.js");
-const platforms = require("./platforms.js");;
+const platforms = require("./platforms.js");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -9,6 +9,9 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.get("/", (req, res) => {
+  res.send({ message: "Okey" });
+});
 router.use("/videogames", games);
 router.use("/genres", genres);
 router.use("/platforms", platforms);
